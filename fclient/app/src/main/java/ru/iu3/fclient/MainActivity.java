3fc2a5fe3e6b5a608586f31e897eb1e735b9ae33
ivan.sidyakin <ivan.sidyakin@gmail.com>
2021-02-28 10:38:17 +0300

    Initial commit

---
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,62 @@
+package ru.iu3.fclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnClickMe);
+        btn.setOnClickListener((View v) -> { onButtonClick(v);});
+
+        int res = initRng();
+        Log.i("fclient", "Init Rng = " + res);
+        byte[] v = randomBytes(10);
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+
+    public static byte[] StringToHex(String s) {
+        try {
+            return Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex) {
+
+        }
+        return new byte[0];
+    }
+
+    protected void onButtonClick(View v) {
+        Toast.makeText(this, stringFromJNI(), Toast.LENGTH_SHORT).show();
+    }
+
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
+}
---
+++ libs/spdlog/build/compile.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+#ABI=armaebi-v7a
+ABI=x86
+#ABI=arm64-v8a
+#ABI=x86_64
+
+ANDROID_NDK=$HOME/Library/Android/sdk/ndk-bundle
+TOOL_CHAIN=${ANDROID_NDK}/build/cmake/android.toolchain.cmake
+CMAKE=$HOME/Library/Android/sdk/cmake/3.10.2.4988404/bin/cmake
+
+mkdir -p ${ABI}
+cd ${ABI}
+
+${CMAKE} ../../spdlog -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 \
+-DANDROID_ABI=${ABI} -DCMAKE_TOOLCHAIN_FILE=${TOOL_CHAIN} -DCMAKE_CXX_FLAGS=-DSPDLOG_COMPILED_LIB
+
+${CMAKE} --build .
+
